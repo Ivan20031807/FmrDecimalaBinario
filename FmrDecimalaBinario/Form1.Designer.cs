bafeafc483f09840978f65c3e3a3936b85e42b85
Ivan.cervantes <ivancervantesquispe@hotmail.com>
2021-11-23 02:56:54 -0500

    Agregar archivos de proyecto.

---
+++ FmrDecimalaBinario/Form1.Designer.cs
@@ -0,0 +1,196 @@
+﻿
+namespace FmrDecimalaBinario
+{
+    partial class fmrdecimalabinario
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtNumero = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.Encendido_o_apagado1 = new System.Windows.Forms.CheckBox();
+            this.Encendido_o_apagado2 = new System.Windows.Forms.CheckBox();
+            this.Encendido_o_apagado3 = new System.Windows.Forms.CheckBox();
+            this.Encendido_o_apagado4 = new System.Windows.Forms.CheckBox();
+            this.Encendido_o_apagado5 = new System.Windows.Forms.CheckBox();
+            this.Encendido_o_apagado6 = new System.Windows.Forms.CheckBox();
+            this.Encendido_o_apagado7 = new System.Windows.Forms.CheckBox();
+            this.Encendido_o_apagado8 = new System.Windows.Forms.CheckBox();
+            this.bntCalcular = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(33, 33);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(90, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Ingresa un entero";
+            // 
+            // txtNumero
+            // 
+            this.txtNumero.Location = new System.Drawing.Point(151, 30);
+            this.txtNumero.Name = "txtNumero";
+            this.txtNumero.Size = new System.Drawing.Size(100, 20);
+            this.txtNumero.TabIndex = 1;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.Encendido_o_apagado8);
+            this.groupBox1.Controls.Add(this.Encendido_o_apagado7);
+            this.groupBox1.Controls.Add(this.Encendido_o_apagado6);
+            this.groupBox1.Controls.Add(this.Encendido_o_apagado5);
+            this.groupBox1.Controls.Add(this.Encendido_o_apagado4);
+            this.groupBox1.Controls.Add(this.Encendido_o_apagado3);
+            this.groupBox1.Controls.Add(this.Encendido_o_apagado2);
+            this.groupBox1.Controls.Add(this.Encendido_o_apagado1);
+            this.groupBox1.Location = new System.Drawing.Point(32, 70);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(457, 139);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Equivalencia en binario";
+            // 
+            // Encendido_o_apagado1
+            // 
+            this.Encendido_o_apagado1.AutoSize = true;
+            this.Encendido_o_apagado1.Location = new System.Drawing.Point(425, 49);
+            this.Encendido_o_apagado1.Name = "Encendido_o_apagado1";
+            this.Encendido_o_apagado1.Size = new System.Drawing.Size(15, 14);
+            this.Encendido_o_apagado1.TabIndex = 0;
+            this.Encendido_o_apagado1.UseVisualStyleBackColor = true;
+            // 
+            // Encendido_o_apagado2
+            // 
+            this.Encendido_o_apagado2.AutoSize = true;
+            this.Encendido_o_apagado2.Location = new System.Drawing.Point(381, 49);
+            this.Encendido_o_apagado2.Name = "Encendido_o_apagado2";
+            this.Encendido_o_apagado2.Size = new System.Drawing.Size(15, 14);
+            this.Encendido_o_apagado2.TabIndex = 1;
+            this.Encendido_o_apagado2.UseVisualStyleBackColor = true;
+            // 
+            // Encendido_o_apagado3
+            // 
+            this.Encendido_o_apagado3.AutoSize = true;
+            this.Encendido_o_apagado3.Location = new System.Drawing.Point(336, 49);
+            this.Encendido_o_apagado3.Name = "Encendido_o_apagado3";
+            this.Encendido_o_apagado3.Size = new System.Drawing.Size(15, 14);
+            this.Encendido_o_apagado3.TabIndex = 2;
+            this.Encendido_o_apagado3.UseVisualStyleBackColor = true;
+            // 
+            // Encendido_o_apagado4
+            // 
+            this.Encendido_o_apagado4.AutoSize = true;
+            this.Encendido_o_apagado4.Location = new System.Drawing.Point(286, 49);
+            this.Encendido_o_apagado4.Name = "Encendido_o_apagado4";
+            this.Encendido_o_apagado4.Size = new System.Drawing.Size(15, 14);
+            this.Encendido_o_apagado4.TabIndex = 3;
+            this.Encendido_o_apagado4.UseVisualStyleBackColor = true;
+            // 
+            // Encendido_o_apagado5
+            // 
+            this.Encendido_o_apagado5.AutoSize = true;
+            this.Encendido_o_apagado5.Location = new System.Drawing.Point(235, 49);
+            this.Encendido_o_apagado5.Name = "Encendido_o_apagado5";
+            this.Encendido_o_apagado5.Size = new System.Drawing.Size(15, 14);
+            this.Encendido_o_apagado5.TabIndex = 4;
+            this.Encendido_o_apagado5.UseVisualStyleBackColor = true;
+            // 
+            // Encendido_o_apagado6
+            // 
+            this.Encendido_o_apagado6.AutoSize = true;
+            this.Encendido_o_apagado6.Location = new System.Drawing.Point(175, 49);
+            this.Encendido_o_apagado6.Name = "Encendido_o_apagado6";
+            this.Encendido_o_apagado6.Size = new System.Drawing.Size(15, 14);
+            this.Encendido_o_apagado6.TabIndex = 5;
+            this.Encendido_o_apagado6.UseVisualStyleBackColor = true;
+            // 
+            // Encendido_o_apagado7
+            // 
+            this.Encendido_o_apagado7.AutoSize = true;
+            this.Encendido_o_apagado7.Location = new System.Drawing.Point(121, 49);
+            this.Encendido_o_apagado7.Name = "Encendido_o_apagado7";
+            this.Encendido_o_apagado7.Size = new System.Drawing.Size(15, 14);
+            this.Encendido_o_apagado7.TabIndex = 6;
+            this.Encendido_o_apagado7.UseVisualStyleBackColor = true;
+            // 
+            // Encendido_o_apagado8
+            // 
+            this.Encendido_o_apagado8.AutoSize = true;
+            this.Encendido_o_apagado8.Location = new System.Drawing.Point(59, 49);
+            this.Encendido_o_apagado8.Name = "Encendido_o_apagado8";
+            this.Encendido_o_apagado8.Size = new System.Drawing.Size(15, 14);
+            this.Encendido_o_apagado8.TabIndex = 7;
+            this.Encendido_o_apagado8.UseVisualStyleBackColor = true;
+            // 
+            // bntCalcular
+            // 
+            this.bntCalcular.Location = new System.Drawing.Point(36, 240);
+            this.bntCalcular.Name = "bntCalcular";
+            this.bntCalcular.Size = new System.Drawing.Size(149, 42);
+            this.bntCalcular.TabIndex = 3;
+            this.bntCalcular.Text = "Calcular";
+            this.bntCalcular.UseVisualStyleBackColor = true;
+            this.bntCalcular.Click += new System.EventHandler(this.bntCalcular_Click);
+            // 
+            // fmrdecimalabinario
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(666, 441);
+            this.Controls.Add(this.bntCalcular);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.txtNumero);
+            this.Controls.Add(this.label1);
+            this.Name = "fmrdecimalabinario";
+            this.Text = "Convierte un entero en binario";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtNumero;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.CheckBox Encendido_o_apagado8;
+        private System.Windows.Forms.CheckBox Encendido_o_apagado7;
+        private System.Windows.Forms.CheckBox Encendido_o_apagado6;
+        private System.Windows.Forms.CheckBox Encendido_o_apagado5;
+        private System.Windows.Forms.CheckBox Encendido_o_apagado4;
+        private System.Windows.Forms.CheckBox Encendido_o_apagado3;
+        private System.Windows.Forms.CheckBox Encendido_o_apagado2;
+        private System.Windows.Forms.CheckBox Encendido_o_apagado1;
+        private System.Windows.Forms.Button bntCalcular;
+    }
+}
+
---
+++ FmrDecimalaBinario/Form1.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FmrDecimalaBinario
+{
+    public partial class fmrdecimalabinario : Form
+    {
+        public fmrdecimalabinario()
+        {
+            InitializeComponent();
+        }
+
+        private void bntCalcular_Click(object sender, EventArgs e)
+        {
+            int numero = int.Parse(this.txtNumero.Text);
+            int[] res = binario(numero);
+            Console.WriteLine("Inicio");
+            for (int i=res.Length-1; i>=0; i--)
+            {
+                Console.Write("{0}" , res[i]);
+            }
+            Console.WriteLine("Fin");
+            if (res[0] == 1)
+            {
+                this.Encendido_o_apagado1.Checked = true;
+            }
+            else
+            {
+                this.Encendido_o_apagado1.Checked = false;
+            }
+            if (res[1] == 1)
+            {
+                this.Encendido_o_apagado2.Checked = true;
+            }
+            else
+            {
+                this.Encendido_o_apagado2.Checked = false;
+            }
+            if (res[2] == 1)
+            {
+                this.Encendido_o_apagado3.Checked = true;
+            }
+            else
+            {
+                this.Encendido_o_apagado3.Checked = false;
+            }
+            if (res[3] == 1)
+            {
+                this.Encendido_o_apagado4.Checked = true;
+            }
+            else
+            {
+                this.Encendido_o_apagado4.Checked = false;
+            }
+            if (res[4] == 1)
+            {
+                this.Encendido_o_apagado5.Checked = true;
+            }
+            else
+            {
+                this.Encendido_o_apagado5.Checked = false;
+            }
+            if (res[5] == 1)
+            {
+                this.Encendido_o_apagado6.Checked = true;
+            }
+            else
+            {
+                this.Encendido_o_apagado6.Checked = false;
+            }
+            if (res[6] == 1)
+            {
+                this.Encendido_o_apagado7.Checked = true;
+            }
+            else
+            {
+                this.Encendido_o_apagado7.Checked = false;
+            }
+            if (res[7] == 1)
+            {
+                this.Encendido_o_apagado8.Checked = true;
+            }
+            else
+            {
+                this.Encendido_o_apagado8.Checked = false;
+            }
+        }
+        private int [] binario(int num)
+        {
+            int [] bin = {0, 0, 0, 0, 0, 0, 0, 0 };
+            //int[] bin = new int[8];
+            int i = 0;
+            int resto = 0;
+            while (num > 0)
+            {
+                resto = num % 2;
+                bin[i++] = resto;
+                num = num / 2;
+                Console.WriteLine("{0}", resto);
+            }
+            return bin;
+        }
+    } 
+    
+}
